a8b2b768abd23a2adff8963dd282fa844dfa2827
Túlio Henrique Santos Mendes <tulio.mendes@sensedia.com>
2023-05-02 16:21:39 -0300

    teste unitário RectangleServiceImpl

--- src/main/java/com/sensedia/mentoria/factory/request/CalculateAreaRequest.java
+++ src/main/java/com/sensedia/mentoria/factory/request/CalculateAreaRequest.java
@@ -3,9 +3,11 @@ package com.sensedia.mentoria.factory.request;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.sensedia.mentoria.factory.dto.GeometricShapes;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CalculateAreaRequest {
     private GeometricShapes geometricShapes;
---
+++ src/test/java/com/sensedia/mentoria/factory/service/RectangleServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.sensedia.mentoria.factory.service;
+
+import com.sensedia.mentoria.factory.dto.Area;
+import com.sensedia.mentoria.factory.dto.Rectangle;
+import com.sensedia.mentoria.factory.request.CalculateAreaRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RectangleServiceImplTest {
+
+    @InjectMocks
+    RectangleServiceImpl rectangleService;
+
+    @Test
+    void testCalculateArea() {
+        CalculateAreaRequest areaRequest = CalculateAreaRequest.builder()
+                .geometricShapes(Rectangle.builder()
+                        .base(5)
+                        .height(6)
+                        .build())
+                .build();
+        Area area = rectangleService.calculateArea(areaRequest);
+
+        Assertions.assertNotNull(area);
+        Assertions.assertEquals(60, area.getArea());
+    }
+
+}
