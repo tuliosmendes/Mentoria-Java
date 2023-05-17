58ff013e8036d631f70f8ed253467fc7cdbf180c
Túlio Henrique Santos Mendes <tulio.mendes@sensedia.com>
2023-05-17 14:39:52 -0300

    Update GeometricShapesControllerTest.java

--- src/test/java/com/sensedia/mentoria/factory/rest/GeometricShapesControllerTest.java
+++ src/test/java/com/sensedia/mentoria/factory/rest/GeometricShapesControllerTest.java
@@ -1,25 +1,17 @@
 package com.sensedia.mentoria.factory.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sensedia.mentoria.factory.dto.GeometricShapes;
-import com.sensedia.mentoria.factory.dto.GeometricShapesType;
-import com.sensedia.mentoria.factory.dto.Square;
-import com.sensedia.mentoria.factory.request.CalculateAreaRequest;
 import com.sensedia.mentoria.factory.utils.MentoriaTestUtils;
-import com.sensedia.mentoria.factory.utils.MentoriaUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(GeometricShapesController.class)
 public class GeometricShapesControllerTest {
     // https://howtodoinjava.com/spring-boot2/testing/spring-boot-mockmvc-example/
@@ -34,7 +26,7 @@ public class GeometricShapesControllerTest {
 
         String expectedJson = "{\"area\":25}";
 
-        mvc.perform( MockMvcRequestBuilders
+        mvc.perform(MockMvcRequestBuilders
                         .post("/mentoria-java/example/factory")
                         .content(requestJson)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -43,7 +35,7 @@ public class GeometricShapesControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.area").value(25))
 
                 .andExpect(MockMvcResultMatchers.content().json(expectedJson));
-        }
+    }
 
     @Test
     public void postGeometricShapeRectangle() throws Exception {
@@ -52,7 +44,7 @@ public class GeometricShapesControllerTest {
 
         String expectedJson = "{\"area\":20}";
 
-        mvc.perform( MockMvcRequestBuilders
+        mvc.perform(MockMvcRequestBuilders
                         .post("/mentoria-java/example/factory")
                         .content(requestJson)
                         .contentType(MediaType.APPLICATION_JSON)
